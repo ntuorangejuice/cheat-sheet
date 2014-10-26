2e84c3ff5095b61c1b4032b864de2ca4148887ed
Zhou Xinzi <imdreamrunner@gmail.com>
2014-10-26 16:51:11 +0800

    change template

--- README.md
+++ README.md
@@ -32,17 +32,32 @@
 #define SHOW_B(x, y) {cout << #x << " = " << x << ", " << #y << " = " << y << endl;}
 #define SHOW_C(x, y, z) {cout << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;}
 #define REACH_HERE {cout << "REACH_HERE!" << endl;}
+#define ASSERT(x) { assert(x); cout << #x << endl; }
 
 const double E = 1e-8;
 const double PI = acos(-1);
 
 using namespace std;
 
+int test = 0, pass = 0;
+
+void assert(bool result) {
+    test++; pass+=result;
+    cout << (result ? "PASS: " : "FAIL: ");
+}
+
+
 int main() {
     ios::sync_with_stdio(false);
-    /*
-     * Main program.
-     */
+
+    int x = 0;
+    ASSERT(x==0)
+    ASSERT((x = SET_BIT(x, 3))==4)
+    ASSERT((x = CLR_BIT(x, 3))==0)
+
+    cout << "Total test: " << test << endl;
+    cout << "Passed: " << pass << " (" << 100.0*pass/test << "%%)" << endl;
+
     return 0;
 }
 ```
--- sources/teamplate.cc
+++ sources/teamplate.cc
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <string>
 #include <cstring>
+#include <cmath>
 #include <algorithm>
 #include <climits>
 #include <stack>
@@ -8,35 +8,44 @@
 #include <vector>
 #include <set>
 #include <map>
-#include <cmath>
+#include <list>
 
 /*
  * n: number
  * i: LSB starting from 1
  */
+#define GET_BIT(n, i) (((n) & (1 << ((i)-1))) >> ((i)-1))
 #define SET_BIT(n, i) ((n) | (1 << ((i)-1)))
 #define CLR_BIT(n, i) ((n) & ~(1 << ((i)-1)))
-#define GET_BIT(n, i) (((n) & (1 << ((i)-1))) >> ((i)-1))
-#define SHOW(x) {cout << #x << " = " << x << endl;}
+#define SHOW_A(x) {cout << #x << " = " << x << endl;}
+#define SHOW_B(x, y) {cout << #x << " = " << x << ", " << #y << " = " << y << endl;}
+#define SHOW_C(x, y, z) {cout << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;}
+#define REACH_HERE {cout << "REACH_HERE!" << endl;}
+#define ASSERT(x) { assert(x); cout << #x << endl; }
+
 const double E = 1e-8;
 const double PI = acos(-1);
 
 using namespace std;
 
+int test = 0, pass = 0;
+
+void assert(bool result) {
+    test++; pass+=result;
+    cout << (result ? "PASS: " : "FAIL: ");
+}
+
+
 int main() {
     ios::sync_with_stdio(false);
 
-    /*
-     * Begin test cases.
-     */
-    // Bit operation shortcut.
     int x = 0;
-    cout << "X = " << x << endl;
-    cout << "SET_BIT " << (x = SET_BIT(x, 3)) << endl;
-    cout << "CLR_BIT " << (x = CLR_BIT(x, 3)) << endl;
-    /*
-     * End test cases.
-     */
+    ASSERT(x==0)
+    ASSERT((x = SET_BIT(x, 3))==4)
+    ASSERT((x = CLR_BIT(x, 3))==0)
+
+    cout << "Total test: " << test << endl;
+    cout << "Passed: " << pass << " (" << 100.0*pass/test << "%%)" << endl;
 
     return 0;
 }
