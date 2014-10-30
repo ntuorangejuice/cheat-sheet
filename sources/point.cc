50b8d1f2ec3763d5f16dcf8ba9c7157849dddc60
Zhou Xinzi <imdreamrunner@gmail.com>
2014-10-30 14:01:09 +0800

    points

--- sources/point.cc
+++ sources/point.cc
@@ -32,17 +32,28 @@ struct point {
         return x*b.y - y*b.x;
     }
 
-    bool at_right_of(const point& that, const point& base) {
-        point vec_self = {x - base.x, y - base.y};
-        point vec_that = {that.x - base.x, that.y - base.y};
+    bool at_right_of(const point& a, const point& b) const {
+        // a: relative point, b: base
+        point vec_self = {x - b.x, y - b.y};
+        point vec_that = {a.x - b.x, a.y - b.y};
         long product = vec_self * vec_that;
         if (product>0) return true;
         if (product==0 && vec_self.length()>vec_that.length()) return true;
         return false;
     }
 
-    double distance_to(const point& b) {
+    double to_point(const point& b) const {
         return sqrt(pow(x-b.x,2) + pow(y-b.y,2));
+    }
+
+    double to_segment(const point& a, const point& b) const {
+        double len_ab = a.to_point(b);
+        if (abs(len_ab)<E) return to_point(a);
+        double r = ((a.y-y)*(a.y-b.y) - (a.x-x)*(a.x-b.x))/pow(len_ab,2);
+        if (r>1 || r<0) return min(to_point(a), to_point(b));
+        // projection of p is on extension of AB
+        r = ((a.y - y)*(b.y - y) - (a.x - x)*(b.y - a.y))/pow(len_ab,2);
+        return fabs(r*len_ab);
     }
 };
 
@@ -51,7 +62,7 @@ int main() {
 
     point a = {1, 1};
     point b = {2, 2};
-    ASSERT(abs(a.distance_to(b) - sqrt(2)) < E);
+    ASSERT(abs(a.to_point(b) - sqrt(2)) < E);
     ASSERT(a.at_right_of(b, (point){0,0}) == false)
     ASSERT(a.at_right_of((point){1,2}, (point){0,0}) == true)
 
